fb6a70cc4c7ec977fb2edd0de3526e541c2473b4
ruspian <ruspianntb@gmail.com>
2024-11-02 23:16:54 +0800

    mengubah data yang ada di database

---
+++ src/users/dtos/update-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsString, IsOptional } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @IsOptional()
+  password?: string;
+}
