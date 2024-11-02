806e223ae2879c3fe9c1705ef13068bcd5e2b506
ruspian <ruspianntb@gmail.com>
2024-11-02 20:39:56 +0800

    proses menyimpan data user melalui service dan repository

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { createUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post()
   createUser(@Body() body: createUserDto) {
-    return body;
+    this.usersService.create(body.name, body.email, body.password);
   }
 }
