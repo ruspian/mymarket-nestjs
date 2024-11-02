e343b92d1c7613e0d65e757b83d0c63855d1f2c7
ruspian <ruspianntb@gmail.com>
2024-11-03 00:01:49 +0800

    menambahkan interceptor untuk menyembunyikan data response

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,16 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+export class SerializeInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    console.log('Before...');
+    return next.handle().pipe(
+      map((data: any) => {
+        console.log('After...');
+      }),
+    );
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,12 +13,13 @@ import {
 import { createUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @UseInterceptors(ClassSerializerInterceptor)
+  @UseInterceptors(SerializeInterceptor)
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.findAll(email);
@@ -29,7 +30,7 @@ export class UsersController {
     this.usersService.create(body.name, body.email, body.password);
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
+  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
