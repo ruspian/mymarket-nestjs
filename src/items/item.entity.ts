cfa8f37efa9e531f552666d269d55b3936d2cb2d
ruspian <ruspianntb@gmail.com>
2024-11-02 20:03:37 +0800

    membuat entitas item dan melihat isi dari hasil membuat entitas

---
+++ src/items/item.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  location: string;
+
+  @Column()
+  category: string;
+}
