0c6a900740cedce2cf3973f14374c19504094116
ruspian <ruspianntb@gmail.com>
2024-11-03 00:12:03 +0800

    menguraii data response melalui dto pada interceptor ini cara yang direkomendasikan untuk menyambunyikan user

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,15 +1,18 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
 import { map, Observable } from 'rxjs';
+import { UserDto } from '../users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    console.log('Before...');
     return next.handle().pipe(
       map((data: any) => {
-        console.log('After...');
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
       }),
     );
   }
