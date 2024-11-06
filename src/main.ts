6a72a80a50c4e5251f5d7a2a69c30b589ad1ab39
ruspian <ruspianntb@gmail.com>
2024-11-06 23:51:58 +0800

    setup session dan cara kerja cookie

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // cookie session
+  app.use(
+    cookieSession({
+      keys: ['qwertyuiop'],
+    }),
+  );
+
+  // validation
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
