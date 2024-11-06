c0d6268a744176c418e33466a0188dc34d6a9367
ruspian <ruspianntb@gmail.com>
2024-11-06 23:11:08 +0800

    fungsi registrasi dan cara mengamankan password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async register(name: string, email: string, password: string) {
+    // cek apakah ada user dengan email yang sama
+    const users = await this.usersService.findAll(email);
+
+    if (users.length) {
+      throw new BadRequestException('Email sudah digunakan!');
+    }
+
+    // hash password user
+
+    // simpan user ke database
+  }
 }
