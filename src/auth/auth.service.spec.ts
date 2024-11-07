7719773c222fa181be69768cda0e5e713b4c2a50
ruspian <ruspianntb@gmail.com>
2024-11-07 22:10:53 +0800

    menguji method register di auuth service harus bekerja dengan benar

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,21 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
+import { User } from 'src/users/user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     // membuat fake user service untuk testing
-    const fakeUsersService = {
-      find: () => Promise.resolve([]),
-      create: () =>
-        Promise.resolve({
-          id: 1,
-          name: 'test',
-          email: 'test@test',
-          password: 'test',
-        }),
+    const fakeUsersService: Partial<UsersService> = {
+      findAll: () => Promise.resolve([]),
+      create: (name: string, email: string, password: string) => {
+        return Promise.resolve({ id: 1, name, email, password } as User);
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -33,5 +30,22 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  // membuat test untuk register
+  it('should create an user', async () => {
+    const user = await service.register(
+      'name1',
+      'email@email.com',
+      'password1',
+    );
+
+    // memastikan password sudah di hash
+    expect(user.password).not.toEqual('password');
+
+    const [salt, hash] = user.password.split('.');
+
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
   });
 });
