c6317badf1417320019d91818364d10638a1d431
ruspian <ruspianntb@gmail.com>
2024-11-08 00:56:23 +0800

    menguji metthod login berhasil di auth controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -2,16 +2,23 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
+import { User } from '../users/user.entity';
 
 describe('AuthController', () => {
   let controller: AuthController;
+
   let fakeUsersService: Partial<UsersService>;
   let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
     fakeUsersService = {};
-    fakeAuthService = {};
+    fakeAuthService = {
+      login: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
+    };
 
+    // menyiapkan mock module
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
@@ -31,5 +38,25 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  // tes untuk user login
+  it('should user login', async () => {
+    const session = {
+      userId: 10,
+    };
+
+    const user = await controller.login(
+      { email: 'email@email.com', password: 'emailpass' },
+      session,
+    );
+
+    expect(user).toEqual({
+      id: 1,
+      email: 'email@email.com',
+      password: 'emailpass',
+    });
+
+    expect(session.userId).toEqual(1);
   });
 });
