8a9e34c78ee1051d5381c45ae83cc1db175e193a
ruspian <ruspianntb@gmail.com>
2024-11-07 21:46:21 +0800

    setup awal file unit test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+    // membuat fake user service untuk testing
+    const fakeUsersService = {
+      find: () => Promise.resolve([]),
+      create: () =>
+        Promise.resolve({
+          id: 1,
+          name: 'test',
+          email: 'test@test',
+          password: 'test',
+        }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
