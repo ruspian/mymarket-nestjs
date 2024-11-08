b53de02b101752c17b700bc599412dd12c75b1d8
ruspian <ruspianntb@gmail.com>
2024-11-08 23:45:06 +0800

    mencoba membuat data item dengan status approved false

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { createItemDto } from './dtos/create-item.dto';
 import { Item } from './item.entity';
@@ -15,5 +15,16 @@ export class ItemsService {
     const newItem = this.itemsRepository.create(item);
     newItem.user = user;
     return this.itemsRepository.save(newItem);
+  }
+
+  async approveItem(id: number, approved: boolean) {
+    const item = await this.itemsRepository.findOneBy({ id });
+
+    if (!item) {
+      throw new NotFoundException('Item tidak ditemukan!');
+    }
+
+    item.approved = approved;
+    return this.itemsRepository.save(item);
   }
 }
