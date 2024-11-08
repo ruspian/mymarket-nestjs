9f5b1e7be531830962f51ec63d827e1956c4fbad
ruspian <ruspianntb@gmail.com>
2024-11-08 22:42:56 +0800

    membuat servicec create item

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { createItemDto } from './dtos/create-item.dto';
+import { Item } from './item.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  constructor(
+    @InjectRepository(Item) private itemsRepository: Repository<Item>,
+  ) {}
+
+  create(body: createItemDto) {
+    const newItem = this.itemsRepository.create(body);
+    return this.itemsRepository.save(newItem);
+  }
+}
